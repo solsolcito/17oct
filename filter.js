73a0c64d60f612e04f54eafa3780f49eefb9ec21
solange alarcon <solealarconmelinao@gmail.com>
2022-10-17 10:50:37 -0300

    iteradores

---
+++ filter.js
@@ -0,0 +1,10 @@
+const mayor5 =(element) => {
+    return element >5 
+    
+   
+   }
+   
+   const arr0 = [1,5,6,8].filter (mayor5)
+   
+       console.log(arr0)
+       
---
+++ main.js
@@ -0,0 +1,31 @@
+const sol = (element,index,c) => {
+    console.log(element,index, c)
+    return element * 2 
+}
+
+const arr1 = [1,5,6,8].map (sol)
+
+    console.log(arr1)
+
+const mayorA5 =(element) =>{
+ if(element >=5 ){
+    
+    return element
+ }else{
+
+    return 0
+ }
+ 
+
+}
+
+const arr = [1,5,6,8].map (mayorA5)
+
+    console.log(arr)
+
+
+    
+
+
+
+
